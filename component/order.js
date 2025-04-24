e2e391fa19f55dfefc3066f09747bb69cf7d7bfd
HN721 <119390246+HN721@users.noreply.github.com>
2025-04-24 17:47:46 +0700

    v1-done

--- component/order.js
+++ component/order.js
@@ -1,6 +1,7 @@
 import { input } from "@inquirer/prompts";
 
 export default async function addOrder(index, item) {
+  let cart = [];
   const answer = await input({ message: `Apakah Kamu Yakin? (Y/N):` });
   if (answer === "Y" || answer === "y") {
     console.log(
@@ -11,7 +12,12 @@ export default async function addOrder(index, item) {
         console.log(
           `${item[index].nama} dengan harga ${item[index].harga} Berhasil Ditambahkan ke Keranjang`
         ),
-      1000
+      2000
     );
+    cart.push(item[index].nama, item[index].harga);
+    const urcart = await input({ message: "Lihat Keranjang? (Y/N)" });
+    if (urcart === "Y" || urcart === "y") {
+      console.log(`Isi Keranjang-Mu ${cart[0]} Rp.${cart[1]}`);
+    }
   }
 }
